8dc45c8a3665ae8e044d37a65c8de145553388e0
Milad_Fotoohenajd <milad.fotoohnejad@gmail.com>
2025-10-23 12:32:54 -0700

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,16 +1,17 @@
-import type {NextConfig} from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: { optimizeCss: false },
   eslint: {
     // Let the build succeed even if there are ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**.cdn.sanity.io' },
-      { protocol: 'https', hostname: 'cdn.sanity.io' }
-    ]
-  }
-}
+      { protocol: "https", hostname: "**.cdn.sanity.io" },
+      { protocol: "https", hostname: "cdn.sanity.io" },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
